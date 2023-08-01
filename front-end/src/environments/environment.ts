daaa20605cbc6095cdeb79af46d3aa64acb6745e
Abel3581 <marzoa3581@gmail.com>
2023-08-01 17:40:07 -0300

    act

--- front-end/src/environments/environment.ts
+++ front-end/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   //apiResrURL: 'http://localhost:8080/api/v1'
   apiResrURL: 'https://powerfit-app.azurewebsites.net/api/v1'
 };
